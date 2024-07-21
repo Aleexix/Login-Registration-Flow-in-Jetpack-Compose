16e564bcc4a70591eb6443e357d066ed99fc5102
Alexix <aorostegui2@gmail.com>
2024-07-21 10:37:51 -0500

    Se crean las funciones HeadingTextComponents y  MyTextField con composable.

--- app/src/main/java/co/edu/sena/login2902081/components/AppComponents.kt
+++ app/src/main/java/co/edu/sena/login2902081/components/AppComponents.kt
@@ -2,8 +2,14 @@ package co.edu.sena.login2902081.components
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
@@ -13,6 +19,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import co.edu.sena.login2902081.R
+import co.edu.sena.login2902081.ui.theme.Primary
 
 @Composable
 fun NormalTextComponents(value:String){
@@ -20,9 +27,9 @@ fun NormalTextComponents(value:String){
         text = value,
         modifier = Modifier
             .fillMaxWidth()
-            .heightIn(min = 80.dp),
+            .heightIn(min = 40.dp),
         style = TextStyle(
-            fontSize = 18.sp,
+            fontSize = 24.sp,
             fontWeight = FontWeight.Normal,
             fontStyle = FontStyle.Normal
         )
@@ -33,12 +40,12 @@ fun NormalTextComponents(value:String){
 }
 
 @Composable
-fun HeadingTextComponents(value:String){
+    fun HeadingTextComponents(value:String){
     Text(
         text = value,
         modifier = Modifier
             .fillMaxWidth()
-            .heightIn(min = 80.dp),
+            .heightIn(),
         style = TextStyle(
             fontSize = 30.sp,
             fontWeight = FontWeight.Bold,
@@ -48,4 +55,26 @@ fun HeadingTextComponents(value:String){
         textAlign = TextAlign.Center
 
     )
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyTextField(labelValue: String){
+    val textValue = remember{
+        mutableStateOf("")
+    }
+
+    OutlinedTextField(
+        label = {Text(text =labelValue)},
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = Primary,
+            focusedLabelColor = Primary,
+            cursorColor = Primary
+        ),
+        keyboardOptions = KeyboardOptions.Default,
+        value = textValue.value,
+        onValueChange = {
+            textValue.value = it
+        } )
 }
