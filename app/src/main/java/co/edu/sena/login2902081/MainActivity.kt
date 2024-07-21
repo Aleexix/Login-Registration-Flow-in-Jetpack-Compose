59dfd2239786f4fb6e124234b7a3d240c7b85a07
Alexix <aorostegui2@gmail.com>
2024-07-21 18:11:40 -0500

    Se coloca la funcion SignUpScreen().

--- app/src/main/java/co/edu/sena/login2902081/MainActivity.kt
+++ app/src/main/java/co/edu/sena/login2902081/MainActivity.kt
@@ -4,14 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import co.edu.sena.login2902081.app.PosOficceApp
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import co.edu.sena.login2902081.app.PostOficceApp
+import co.edu.sena.login2902081.screens.SignUpScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            PosOficceApp()
+            PostOficceApp()
+            SignUpScreen()
         }
     }
+}
+
+@Preview
+@Composable
+fun DefaultPreview(){
+    PostOficceApp()
 }
