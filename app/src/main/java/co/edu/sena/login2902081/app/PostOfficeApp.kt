01ea54f6c8048110eef1d670b22e8433ddc62e67
Alexix <aorostegui2@gmail.com>
2024-07-21 18:09:19 -0500

    Se ajusto un poco el nombre de la funcion.

--- app/src/main/java/co/edu/sena/login2902081/app/PostOfficeApp.kt
+++ app/src/main/java/co/edu/sena/login2902081/app/PostOfficeApp.kt
@@ -8,7 +8,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 
 @Composable
-fun PosOficceApp(){
+fun PostOficceApp(){
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = Color.White
