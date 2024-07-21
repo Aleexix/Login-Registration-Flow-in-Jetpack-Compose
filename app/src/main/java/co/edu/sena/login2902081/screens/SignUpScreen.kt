90af4c8db826bc173e5bea689281608b84e726ca
Alexix <aorostegui2@gmail.com>
2024-07-21 13:37:43 -0500

    Se coloca el icono de email con la cadena de texto de la misma

--- app/src/main/java/co/edu/sena/login2902081/screens/SignUpScreen.kt
+++ app/src/main/java/co/edu/sena/login2902081/screens/SignUpScreen.kt
@@ -2,12 +2,15 @@ package co.edu.sena.login2902081.screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -28,7 +31,18 @@ fun SignUpScreen() {
         Column(modifier = Modifier.fillMaxSize()) {
             NormalTextComponents(value = stringResource(id = R.string.hello))
             HeadingTextComponents(value = stringResource(id = R.string.create_account))
-            MyTextFieldComponent(labelValue = stringResource(id = R.string.first_name))
+            Spacer(modifier = Modifier.height(20.dp))
+            MyTextFieldComponent(labelValue = stringResource(id = R.string.first_name),
+                painterResource(id = R.drawable.profile)
+            )
+            MyTextFieldComponent(
+                labelValue = stringResource(id = R.string.last_name),
+                painterResource = painterResource(id = R.drawable.profile)
+            )
+            MyTextFieldComponent(
+                labelValue = stringResource(id = R.string.email),
+                painterResource = painterResource(id = R.drawable.email)
+            )
         }
 
     }
