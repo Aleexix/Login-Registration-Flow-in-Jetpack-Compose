4ea1c9dcd769c8a926a9e3cc657ace503eed0b04
Alexix <aorostegui2@gmail.com>
2024-07-21 18:14:42 -0500

    Se crea la funcion  PasswordTextFieldComponent

--- app/src/main/java/co/edu/sena/login2902081/components/AppComponents.kt
+++ app/src/main/java/co/edu/sena/login2902081/components/AppComponents.kt
@@ -1,12 +1,14 @@
-@file:OptIn(ExperimentalMaterial3Api::class)
-
 package co.edu.sena.login2902081.components
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
@@ -15,11 +17,16 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -63,8 +70,9 @@ fun HeadingTextComponents(value: String) {
 }
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MyTextFieldComponent(labelValue: String) {
+fun MyTextFieldComponent(labelValue: String, painterResource: Painter) {
 
     val textValue = remember {
         mutableStateOf("")
@@ -87,7 +95,62 @@ fun MyTextFieldComponent(labelValue: String) {
             textValue.value = it
         },
         leadingIcon ={
-            Icon(painter = painterResource(id = R.drawable.profile), contentDescription ="" )
+            Icon(painter = painterResource, contentDescription ="" )
         }
+    )
+}
+
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PasswordTextFieldComponent(labelValue: String, painterResource: Painter) {
+
+    val password = remember {
+        mutableStateOf("")
+    }
+
+    val passwordVisible = remember {
+        mutableStateOf(false)
+    }
+
+    OutlinedTextField(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(componentShapes.small),
+        label = { Text(text = labelValue) },
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = Primary,
+            focusedLabelColor = Primary,
+            cursorColor = Primary,
+            containerColor = Bgcolor
+        ),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        value = password.value,
+        onValueChange = {
+            password.value = it
+        },
+        leadingIcon ={
+            Icon(painter = painterResource, contentDescription ="" )
+        },
+        trailingIcon = {
+
+            val iconImages = if(passwordVisible.value){
+                Icons.Filled.Visibility
+            } else{
+                Icons.Filled.VisibilityOff
+            }
+
+            val description = if (passwordVisible.value){
+                stringResource(id = R.string.hide_password)
+            }else{
+                stringResource(id = R.string.Show_password)
+            }
+            IconButton(onClick = {passwordVisible.value = !passwordVisible.value}) {
+                Icon(imageVector = iconImages, contentDescription = description )
+                
+            }
+        },
+        visualTransformation = if(passwordVisible.value) VisualTransformation.None else PasswordVisualTransformation()
     )
 }
