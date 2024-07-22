ebc5e6406f03f4e72c75d3854d11663731750fc4
Alexix <aorostegui2@gmail.com>
2024-07-22 10:32:24 -0500

    Se añaden funcionalidades en la funcion PostOficceApp

--- app/src/main/java/co/edu/sena/login2902081/MainActivity.kt
+++ app/src/main/java/co/edu/sena/login2902081/MainActivity.kt
@@ -1,20 +1,19 @@
 package co.edu.sena.login2902081
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import co.edu.sena.login2902081.app.PostOficceApp
-import co.edu.sena.login2902081.screens.SignUpScreen
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PostOficceApp()
-            SignUpScreen()
         }
     }
 }
--- app/src/main/java/co/edu/sena/login2902081/app/PostOfficeApp.kt
+++ app/src/main/java/co/edu/sena/login2902081/app/PostOfficeApp.kt
@@ -1,11 +1,16 @@
 package co.edu.sena.login2902081.app
 
 
+import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import co.edu.sena.login2902081.navigation.PostOfficeAppRouter
+import co.edu.sena.login2902081.navigation.Screen
+import co.edu.sena.login2902081.screens.SignUpScreen
+import co.edu.sena.login2902081.screens.TermsAndConditionsScreen
 
 @Composable
 fun PostOficceApp(){
@@ -14,6 +19,18 @@ fun PostOficceApp(){
         color = Color.White
 
         ) {
+
+        Crossfade(targetState = PostOfficeAppRouter.currentScreen) { currentState ->
+            when(currentState.value){
+                is Screen.SignUpScreen ->{
+                    SignUpScreen()
+                }
+                is Screen.TermsAndConditionsScreen -> {
+                    TermsAndConditionsScreen()
+                }
+            }
+            
+        }
 
     }
 }
